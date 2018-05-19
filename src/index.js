04c206963142e23b9fcffb07b24b21098c84d18e
Alastair Netterfield <alastair.netterfield@corpgovrisk.com>
2018-05-19 23:35:55 +0800

    chore: Add reducer and middleware boilerplate

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './components/App';
+import middleware from './middleware';
+import reducer from './reducers';
+
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
